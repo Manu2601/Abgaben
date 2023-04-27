60c3eeaaa03c30c20e09906ef3772de716573fc5
Manu2601 <manraffl@tsn.at>
2023-04-27 16:35:24 +0200

    initial commit

---
+++ Abgaben/src/Sockets/Client.java
@@ -0,0 +1,60 @@
+package Sockets;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+	public static void main(String[] args) {
+		Socket s = null;
+		DataOutputStream dout = null;
+		try {
+			s = new Socket("localhost", 6666);
+			dout = new DataOutputStream(s.getOutputStream());
+			dout.writeUTF(readTerminal());
+			dout.flush();
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			String response = "";
+			while ((response = in.readLine()) != null) {
+				System.out.println(response);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				s.close();
+				dout.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static String readTerminal() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Senden(WRITE) oder Empfangen(LIST):");
+		String sendenEmpfangen = s.nextLine();
+		String rückgabe;
+		if (sendenEmpfangen.equals("WRITE")) {
+			System.out.println("Dateiname eingeben:");
+			String dataname = s.nextLine();
+			System.out.println("Text eingeben:");
+			String text = s.nextLine();
+			s.close();
+			rückgabe = sendenEmpfangen + "#" + dataname + "#" + text;
+		} else if (sendenEmpfangen.equals("LIST")) {
+			s.close();
+			rückgabe = sendenEmpfangen;
+		} else {
+			System.out.println("Falsche Eingabe");
+			rückgabe = "";
+		}
+		return rückgabe;
+	}
+
+}
---
+++ Abgaben/src/Sockets/FileHandling.java
@@ -0,0 +1,42 @@
+package Sockets;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandling {
+    private String str;
+    private long length;
+
+    public FileHandling(String str){
+        this.str = str;
+    }
+
+    public void safeFile() throws IOException {
+        String[] string = str.split("#");
+        File datei = new File(
+                "C:/tmp/Server/"
+                        + string[1]);
+        FileWriter fw = new FileWriter(datei);
+        fw.write(string[2]);
+        fw.flush();
+        fw.close();
+        length = datei.length();
+    }
+
+    public long getLength(){
+        return length;
+    }
+
+    public String getList(){
+        File dir = new File("C:/tmp/Server/");
+        File[] files = dir.listFiles();
+        String rückgabe="";
+        for (File file : files) {
+            if (file.isFile()) {
+                rückgabe+=file.getName() +"\n";
+            }
+        }
+        return rückgabe;
+    }
+}
